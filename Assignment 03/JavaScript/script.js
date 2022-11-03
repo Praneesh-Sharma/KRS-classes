c5b3ee24be5050c6794abfaa5164ef444b30a701
Praneesh Sharma <pegasus30042004@gmail.com>
2022-11-03 15:36:44 +0530

    added assignment 3 files

---
+++ Assignment 03/JavaScript/script.js
@@ -0,0 +1,5 @@
+var first_name = "Praneesh"
+var last_name = "Sharma"
+var birth = "2004"
+
+console.log("Hi, my name is", first_name, last_name, ", I'm", 2022-birth, "years old and I'm learning JavaScript");
