5c6b0a2141ac6aabd18a6ece5abe8ca177e22ac7
Praneesh Sharma <pegasus30042004@gmail.com>
2022-11-06 14:29:47 +0530

    finally ended

--- Day 05/JavaScript/closure.js
+++ Day 05/JavaScript/closure.js
@@ -2,15 +2,28 @@
 // with references to its surrounding state (the lexical environment). In other words,
 // a closure gives you access to an outer function's scope from an inner function.
 
-function x(){
-    var a=7;
-    function y(){
-        console.log(a); //7
-    }
-    // y();
-    return y
+// function x(){
+//     var a=7;
+//     function y(){
+//         console.log(a); //7
+//     }
+//     // y();
+//     return y
+// }
+// x()
+// var z=x()
+// console.log(z)
+// z()
+
+a()
+blur()
+setTimeout(c, 500) //in milliseconds, setInterval is a callback function
+function a(){
+    console.log("a");
 }
-x()
-var z=x()
-console.log(z)
-z()
+function blur(){
+    console.log("b");
+}
+function c(){
+    console.log("c");
+}
